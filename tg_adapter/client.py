bb80ac35c18ce2fd4273d59029047d10ab071901
stepan <stepan@basharov.net>
2026-06-23 19:50:35 +0300

    swd

--- tg_adapter/client.py
+++ tg_adapter/client.py
@@ -1,6 +1,7 @@
+import asyncio
 from telegram import Bot
 
 bot = Bot(token="YOUR_BOT_TOKEN")
 
 def send_to_telegram(chat_id, text):
-    bot.send_message(chat_id=chat_id, text=text)
+    asyncio.run(bot.send_message(chat_id=chat_id, text=text))
